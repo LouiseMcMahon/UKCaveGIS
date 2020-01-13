7a9e0949ac96f7d470739da98ab9181ef778a6ab
Louise McMahon <louise@louisemcmahon.co.uk>
2020-01-13 23:02:52 +0000

    fixed bug in start up script

--- app/start.py
+++ app/start.py
@@ -1,6 +1,6 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
-from ukcavegis.spiders import dcaregistry, mendipregistry, scotlandregistry, cnccregistry
+from ukcavegis.spiders import dcaregistry, mendipregistry, scotlandregistry, cnccregistry, cccRegistry, fodccagRegistry, ducRegistry
 from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
@@ -8,7 +8,7 @@ process.crawl(dcaregistry.DcaRegistry)
 process.crawl(mendipregistry.MendipRegistry)
 process.crawl(scotlandregistry.ScotlandRegistry)
 process.crawl(cnccregistry.CnccRegistry)
-process.crawl(cnccregistry.DucRegistry)
-process.crawl(cnccregistry.FodccagRegistry)
-process.crawl(cnccregistry.CccRegistry)
+process.crawl(ducRegistry.DucRegistry)
+process.crawl(fodccagRegistry.FodccagRegistry)
+process.crawl(cccRegistry.CccRegistry)
 process.start()
